683ceaacb7fdbc9058bc071b3d7a77109446dfbc
Janishnehyan03 <nehyanjanish@gmail.com>
2022-06-30 09:43:56 +0530

    animation added to btn

--- client/src/LaunchButton.jsx
+++ client/src/LaunchButton.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function LaunchButton() {
+  const [launched, setLaunched] = useState(false);
+  if (launched) {
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 3000);
+  }
   return (
     <div className="btnBody">
       <div class="flex h-screen">
         <div class="m-auto">
-          <Link to={"/"} className="button">
+          <button to={"/"} className="button" onClick={() => setLaunched(true)}>
             <span>LAUNCH </span>
-          </Link>
+          </button>
         </div>
         <h1
           className="text-white mx-auto"
@@ -22,6 +29,20 @@ function LaunchButton() {
           بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ
         </h1>
       </div>
+      {launched && (
+        <h1
+          style={{
+            position: "absolute",
+            top: "60%",
+            left: "35.5%",
+            fontSize: "1.5rem",
+          }}
+          id="successH1"
+          className="text-green-600 font-bold bg-white px-4 py-2"
+        >
+          WEBSITE LAUNCHED SUCCESSFULLY
+        </h1>
+      )}
     </div>
   );
 }
