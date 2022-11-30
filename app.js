a134e60121b5cc874f42e312f617302b60ac68c3
Janish Nehyan <85396611+Janishnehyan03@users.noreply.github.com>
2022-11-30 09:04:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(morgan("dev"));
 
 mongoose.connect(
-  process.env.MONGO_URL,
+  process.env.MONGO_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     console.log("connected");
